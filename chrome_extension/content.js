e29a388c74671543e6d45fcc7ff219731112183f
ladyrick <ladyrick@qq.com>
2020-09-22 17:31:07 +0800

    use script to inject.

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,35 +1,35 @@
 (function () {
-    const html = document.querySelector('html');
-    html.setAttribute('onclick', `window.CrosFetch = (function () {
-    document.addEventListener('response', function (e) {
-        const response_obj = e.detail;
-        document.dispatchEvent(new CustomEvent('cros-fetch-' + response_obj.key, { detail: response_obj }));
-    }, false);
-
-    return (input, init = {}) => {
-        return new Promise((resolve, reject) => {
-            const request = {
-                input: input,
-                init: init,
-                key: Math.random().toString(36).slice(2),
-            };
-            document.dispatchEvent(new CustomEvent('request', { detail: request }));
-            const handler = (e) => {
-                document.removeEventListener('cros-fetch-' + request.key, handler);
-                const response_obj = e.detail;
-                if (response_obj.error) {
-                    reject(new window[response_obj.error.name](response_obj.error.message));
-                } else {
-                    resolve(response_obj.resolve);
+    const script = document.createElement('script');
+    script.innerHTML = `\n\n
+    window.CrosFetch = (function () {
+        document.addEventListener('response', function (e) {
+            const response_obj = e.detail;
+            document.dispatchEvent(new CustomEvent('cros-fetch-' + response_obj.key, { detail: response_obj }));
+        }, false);
+    
+        return (input, init = {}) => {
+            return new Promise((resolve, reject) => {
+                const request = {
+                    input: input,
+                    init: init,
+                    key: Math.random().toString(36).slice(2),
+                };
+                document.dispatchEvent(new CustomEvent('request', { detail: request }));
+                const handler = (e) => {
+                    document.removeEventListener('cros-fetch-' + request.key, handler);
+                    const response_obj = e.detail;
+                    if (response_obj.error) {
+                        reject(new window[response_obj.error.name](response_obj.error.message));
+                    } else {
+                        resolve(response_obj.resolve);
+                    }
                 }
-            }
-            document.addEventListener('cros-fetch-' + request.key, handler);
-        });
-    };
-})();
-document.querySelector('html').removeAttribute('onclick');
-`);
-    html.click();
+                document.addEventListener('cros-fetch-' + request.key, handler);
+            });
+        };
+    })();
+    `;
+    document.documentElement.appendChild(script);
     document.addEventListener('request', function (e) {
         console.log('%cCrosFetch req ' + e.detail.key, 'color: #e88d67', e.detail);
         chrome.runtime.sendMessage('', e.detail, {}, response => {
